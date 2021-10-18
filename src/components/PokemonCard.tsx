c27aef4525e79db05b1a0307566b2b5208cbdf33
alex0208 <alexander.zahariev1996@gmail.com>
2021-10-18 21:59:55 +0200

    Added pokemon card and catalog

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,92 @@
+import {
+    Button,
+    capitalize,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Typography,
+} from '@mui/material';
+
+import { Box, styled } from '@mui/system';
+import { PokemonQuery_pokemon_v2_pokemon_pokemon_v2_pokemonabilities } from '../graphql/generated/PokemonQuery';
+
+interface Props {
+    id: number;
+    name: string;
+    height: number | null;
+    weight: number | null;
+    pokemon_v2_pokemonabilities: PokemonQuery_pokemon_v2_pokemon_pokemon_v2_pokemonabilities[];
+}
+
+const PokemonCard = ({
+    id = 5,
+    name = 'Pokemon',
+    weight,
+    height,
+    pokemon_v2_pokemonabilities: abilities,
+}: Props) => {
+    return (
+        <PCard sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="250"
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+                    alt={name}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h4" component="div">
+                        {capitalize(name)}
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        color="text.secondary"
+                        component="div"
+                    >
+                        Height:
+                        <AttributeValue>{height}</AttributeValue>
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        color="text.secondary"
+                        component="div"
+                    >
+                        Weight:
+                        <AttributeValue>{weight}</AttributeValue>
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        color="text.secondary"
+                        component="div"
+                    >
+                        Abilities:
+                        <AttributeValue>
+                            {abilities
+                                .map((a) => a.pokemon_v2_ability?.name)
+                                .join(', ')}
+                        </AttributeValue>
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    More
+                </Button>
+            </CardActions>
+        </PCard>
+    );
+};
+
+const AttributeValue = styled(Box)({
+    paddingLeft: 5,
+    display: 'inline',
+    fontWeight: 500,
+});
+
+const PCard = styled(Card)({
+    minWidth: 200,
+});
+
+export default PokemonCard;
--- src/routes/PokemonCatalog.tsx
+++ src/routes/PokemonCatalog.tsx
@@ -1,8 +1,89 @@
-import React, { FC } from 'react';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Grid,
+    Card,
+    Button,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Pagination,
+} from '@mui/material';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router';
+import Deck from '../Deck';
+import PokemonCard from '../components/PokemonCard';
+import { useLazyQuery } from '@apollo/client';
+import {
+    PokemonQuery,
+    PokemonQueryVariables,
+} from '../graphql/generated/PokemonQuery';
+import { POKEMON_QUERY } from '../graphql/queries/pokemon';
 
 const PokemonCatalog: FC<RouteComponentProps> = () => {
-    return <div>pokedex</div>;
+    const [pageSize, setPageSize] = useState(20);
+    const [currentPage, setCurrrentPage] = useState(1);
+    const [getData, { data, loading }] = useLazyQuery<
+        PokemonQuery,
+        PokemonQueryVariables
+    >(POKEMON_QUERY, {
+        variables: { limit: pageSize, offset: pageSize * (currentPage - 1) },
+    });
+
+    const handlePageChange = (event: ChangeEvent<unknown>, value: number) => {
+        setCurrrentPage(value);
+    };
+
+    useEffect(() => {
+        getData();
+        console.log(data);
+    }, []);
+
+    return (
+        <>
+            <Grid
+                container
+                alignItems="center"
+                justifyContent="flex-start"
+                spacing={3}
+                px={3}
+                // spacing={{ xs: 2, md: 3 }}
+                // columns={{ xs: 4, sm: 8, md: 12 }}
+            >
+                {data &&
+                    Array.from(data.pokemon_v2_pokemon).map(
+                        (pokemon, index) => (
+                            <Grid
+                                item
+                                // spacing={5}
+                                xs={6}
+                                sm={4}
+                                md={3}
+                                lg={2}
+                                key={index}
+                            >
+                                <PokemonCard
+                                    {...pokemon}
+                                    // id={pokemon.id}
+                                    // name={pokemon.name}
+                                />
+                            </Grid>
+                        )
+                    )}
+            </Grid>
+            <Typography>Page: {currentPage}</Typography>
+            <Pagination
+                count={currentPage + 2}
+                siblingCount={3}
+                boundaryCount={2}
+                page={currentPage}
+                onChange={handlePageChange}
+            />
+        </>
+    );
 };
 
 export default PokemonCatalog;

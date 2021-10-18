58bec1c88e19f29e553b8e37fdf819fd84c2b13b
alex0208 <alexander.zahariev1996@gmail.com>
2021-10-18 22:18:42 +0200

    Apollo codegen

--- src/graphql/generated/PokemonQuery.ts
+++ src/graphql/generated/PokemonQuery.ts
@@ -3,44 +3,47 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
+import { pokemon_v2_pokemon_order_by } from './globalTypes';
+
 // ====================================================
 // GraphQL query operation: PokemonQuery
 // ====================================================
 
 export interface PokemonQuery_pokemon_v2_pokemon_pokemon_v2_pokemonabilities_pokemon_v2_ability {
-  __typename: "pokemon_v2_ability";
-  name: string;
+    __typename: 'pokemon_v2_ability';
+    name: string;
 }
 
 export interface PokemonQuery_pokemon_v2_pokemon_pokemon_v2_pokemonabilities {
-  __typename: "pokemon_v2_pokemonability";
-  /**
-   * An object relationship
-   */
-  pokemon_v2_ability: PokemonQuery_pokemon_v2_pokemon_pokemon_v2_pokemonabilities_pokemon_v2_ability | null;
+    __typename: 'pokemon_v2_pokemonability';
+    /**
+     * An object relationship
+     */
+    pokemon_v2_ability: PokemonQuery_pokemon_v2_pokemon_pokemon_v2_pokemonabilities_pokemon_v2_ability | null;
 }
 
 export interface PokemonQuery_pokemon_v2_pokemon {
-  __typename: "pokemon_v2_pokemon";
-  id: number;
-  name: string;
-  height: number | null;
-  weight: number | null;
-  /**
-   * An array relationship
-   */
-  pokemon_v2_pokemonabilities: PokemonQuery_pokemon_v2_pokemon_pokemon_v2_pokemonabilities[];
+    __typename: 'pokemon_v2_pokemon';
+    id: number;
+    name: string;
+    height: number | null;
+    weight: number | null;
+    /**
+     * An array relationship
+     */
+    pokemon_v2_pokemonabilities: PokemonQuery_pokemon_v2_pokemon_pokemon_v2_pokemonabilities[];
 }
 
 export interface PokemonQuery {
-  /**
-   * fetch data from the table: "pokemon_v2_pokemon"
-   */
-  pokemon_v2_pokemon: PokemonQuery_pokemon_v2_pokemon[];
+    /**
+     * fetch data from the table: "pokemon_v2_pokemon"
+     */
+    pokemon_v2_pokemon: PokemonQuery_pokemon_v2_pokemon[];
 }
 
 export interface PokemonQueryVariables {
-  limit?: number | null;
-  offset?: number | null;
-  _ilike?: string | null;
+    limit?: number | null;
+    offset?: number | null;
+    _ilike?: string | null;
+    order_by?: pokemon_v2_pokemon_order_by[] | null | any;
 }
